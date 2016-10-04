67ffdd37c70f18cb35ea55cec0287a4ad9852048
James Chang <jamesbchang@gmail.com>
2016-10-04 13:42:45 +0900

    Minor style changes

--- src/app/actions/Interpretation.action.js
+++ src/app/actions/Interpretation.action.js
@@ -7,31 +7,30 @@ const actions = Action.createActionsFromNames(['listInterpretation', 'getMap', '
 
 // TODO: Does not have fail response, or always response!!!
 actions.listInterpretation
-    .subscribe(({ data: [model, page, searchData], complete, error }) => {
-        getD2().then(d2 => {
-            const url = `interpretations?fields=id,type,text,created,likes,likedBy[id,name],user[id,name],comments[id,created,text,user[id,name]],chart[id,name,series,category,filterDimensions,relativePeriods,periods],map[id,name],reportTable[id,name,relativePeriods,filterDimensions,rowDimensions,columnDimensions,periods]&page=${page}&pageSize=10${searchData}`;
+.subscribe(({ data: [model, page, searchData], complete, error }) => {
+    getD2().then(d2 => {
+        const url = `interpretations?fields=id,type,text,created,likes,likedBy[id,name],user[id,name],comments[id,created,text,user[id,name]],chart[id,name,series,category,filterDimensions,relativePeriods,periods],map[id,name],reportTable[id,name,relativePeriods,filterDimensions,rowDimensions,columnDimensions,periods]&page=${page}&pageSize=10${searchData}`;
 
-            d2.Api.getApi().get(url)
-				.then(result => {
-    complete(result);
-})
-.catch(error);
-        });
+        d2.Api.getApi().get(url)
+        .then(result => {
+            complete(result);
+        })
+        .catch(error);
     });
-
+});
 
 actions.getMap
-    .subscribe(({ data: [model, mapId], complete }) => {
-        getD2().then(d2 => {
-            const url = `/25/maps/${mapId}.json?fields=mapViews[*,columns[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],rows[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],filters[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],dataDimensionItems,program[id,displayName|rename(name)],programStage[id,displayName|rename(name)],legendSet[id,displayName|rename(name)],!lastUpdated,!href,!created,!publicAccess,!rewindRelativePeriods,!userOrganisationUnit,!userOrganisationUnitChildren,!userOrganisationUnitGrandChildren,!externalAccess,!access,!relativePeriods,!columnDimensions,!rowDimensions,!filterDimensions,!user,!organisationUnitGroups,!itemOrganisationUnitGroups,!userGroupAccesses,!indicators,!dataElements,!dataElementOperands,!dataElementGroups,!dataSets,!periods,!organisationUnitLevels,!organisationUnits,!sortOrder,!topLimit]`;
+.subscribe(({ data: [model, mapId], complete }) => {
+    getD2().then(d2 => {
+        const url = `/25/maps/${mapId}.json?fields=mapViews[*,columns[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],rows[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],filters[dimension,filter,items[dimensionItem|rename(id),dimensionItemType,displayName|rename(name)]],dataDimensionItems,program[id,displayName|rename(name)],programStage[id,displayName|rename(name)],legendSet[id,displayName|rename(name)],!lastUpdated,!href,!created,!publicAccess,!rewindRelativePeriods,!userOrganisationUnit,!userOrganisationUnitChildren,!userOrganisationUnitGrandChildren,!externalAccess,!access,!relativePeriods,!columnDimensions,!rowDimensions,!filterDimensions,!user,!organisationUnitGroups,!itemOrganisationUnitGroups,!userGroupAccesses,!indicators,!dataElements,!dataElementOperands,!dataElementGroups,!dataSets,!periods,!organisationUnitLevels,!organisationUnits,!sortOrder,!topLimit]`;
 
-            d2.Api.getApi().get(url)
-				.then(complete)
-.catch(errorResponse => {
-    console.log(errorResponse);
-				});
+        d2.Api.getApi().get(url)
+        .then(complete)
+        .catch(errorResponse => {
+            console.log(errorResponse);
         });
     });
+});
 
 actions.deleteInterpretation
     .subscribe(({ data: [model, id], complete }) => {
