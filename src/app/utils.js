48c08f15bd2e81fefc20757de7c0cd98b5805191
James Chang <jamesbchang@gmail.com>
2018-05-07 12:35:29 +0900

    changes

--- src/app/utils.js
+++ src/app/utils.js
@@ -40,6 +40,22 @@ export const restUtil = {
             successFunc(result);
         });
     },
+    requestPostHelper(d2Api, url, value, successFunc) {
+        d2Api.post(url, value, { contentType: 'text/plain' })
+            .then(successFunc)
+            .catch(errorResponse => {
+                console.log(errorResponse);
+            });
+    },
+    requestHelper(d2Api, url, value, successFunc, requestType, returnContentType) {
+        const reqType = (requestType === undefined) ? 'POST' : requestType;
+        const returnContType = (returnContentType === undefined) ? 'text/plain' : returnContentType;
+        d2Api.request(reqType, url, value, { contentType: returnContType })
+        .then(successFunc)
+        .catch(errorResponse => {
+            console.log(errorResponse);
+        });
+    },
 };
 
 export const otherUtils = {
